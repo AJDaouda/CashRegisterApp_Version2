ce0ba0eb64615bb7fd2543ba241f9225cb30a60e
AJDaouda <AJ.Daouda@outlook.com>
2021-11-13 21:18:15 -0500

    Version 2

---
+++ CashRegisterApp - Copy/app/src/main/java/com/example/cashregisterapp/ListViewAdapter.java
@@ -0,0 +1,57 @@
+package com.example.cashregisterapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ListViewAdapter extends BaseAdapter {
+
+    StoreManager StoreMng = new StoreManager();
+    List<Product> productList;
+    Context storeContext;
+    //LayoutInflater inflater; 2nd method
+
+
+    public ListViewAdapter(List<Product> list,Context activity_context){
+        this.productList = list;
+        this.storeContext = activity_context;
+        //this.inflater = LayoutInflater.from(storeContext); 2nd method
+    }
+
+    @Override
+    public int getCount() {
+        return StoreMng.listOfProd.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = LayoutInflater.from(storeContext).inflate(R.layout.list_view_item,null);
+        //convertView = inflater.inflate(R.layout.list_view_item,null); 2nd method
+        TextView itemName = (TextView) convertView.findViewById(R.id.prodName);
+        TextView itemQnt = (TextView) convertView.findViewById(R.id.prodQnt);
+        TextView itemPrice = (TextView) convertView.findViewById(R.id.prodPrice);
+        itemName.setText(productList.get(position).getProdName());
+        itemQnt.setText(String.valueOf(productList.get(position).getProdQnt()));
+        // we can also use
+        //itemQnt.setText(productList.get(position).getProdQnt() + "");
+        itemPrice.setText(String.valueOf(productList.get(position).getProdPrice()));
+        return convertView;
+
+    }
+
+}
---
+++ CashRegisterApp - Copy/app/src/main/java/com/example/cashregisterapp/StoreManager.java
@@ -0,0 +1,34 @@
+package com.example.cashregisterapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import java.util.ArrayList;
+
+public class StoreManager {
+    //Create prod array list
+    // connect to custom adapter to show in prod list
+
+    ArrayList<Product> listOfProd = new ArrayList<>();
+
+    Product shirt=new Product("Shirts", 50, 19.99);
+    Product pant =new Product("Pants", 40, 20.44);
+    Product shoe =new Product("Shoes", 30, 10.44);
+    Product hat =new Product("Hats", 10, 5.99);
+    public StoreManager() {
+        this.listOfProd.add(pant);
+        this.listOfProd.add(shirt);
+        this.listOfProd.add(shoe);
+        this.listOfProd.add(hat);
+    }
+
+    //Checks the product inventory when a client wants to buy
+    boolean checkInventory(Product inventory, int clientQnt){
+        if(clientQnt>inventory.getProdQnt()){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
