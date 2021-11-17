c4c771221fa2b1e570353317c092e83ddb3bb906
AJDaouda <AJ.Daouda@outlook.com>
2021-11-17 09:39:18 -0500

    CashRegisterApp_Version2.6

---
+++ CashRegisterApp/app/src/main/java/com/example/cashregisterapp/HistoryDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.cashregisterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_details);
+    }
+}
--- CashRegisterApp/app/src/main/java/com/example/cashregisterapp/ManagerActivity.java
+++ CashRegisterApp/app/src/main/java/com/example/cashregisterapp/ManagerActivity.java
@@ -6,30 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.cashregisterapp.Model.PurchaseHistory;
 
 public class ManagerActivity extends AppCompatActivity {
 
-    Button history;
+    Button history, restock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
-        history = (Button) findViewById(R.id.btn_history);
+        history = (Button) findViewById(R.id.history_btn);
+        restock = (Button) findViewById(R.id.restock_btn);
        /* if (getIntent().hasExtra(HistoryList)){
             // PurchaseHistory shopperPH = getIntent().getExtras().getParcelable("HistoryList");
             // System.out.println(shopperPH.toString());
         }*/
     }
 
-    public void historyClicked(View v){
-    Intent toPurchaseReport = new Intent(this, HistoryReportActivity.class);
-    startActivity(toPurchaseReport);}
-
-    public void restockClicked(View v){
-        Intent toRestockActivity = new Intent(this, RestockActivity.class);
-        startActivity(toRestockActivity);}
-
+    public void btnClicked(View v){
+        if (v.equals(history)) {
+            Intent toPurchaseReport = new Intent(this, HistoryReportActivity.class);
+            startActivity(toPurchaseReport);
+            Toast.makeText(this, "History clicked", Toast.LENGTH_SHORT).show();
+        } else if (v.equals(restock)) {
+            Intent toRestockActivity = new Intent(this, RestockActivity.class);
+            startActivity(toRestockActivity);
+            Toast.makeText(this, "Restock clicked", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
